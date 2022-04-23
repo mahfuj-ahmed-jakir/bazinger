70533d21a6b99235d9e79cdaa9be9bf180c58c63
Mahfuj Ahmed Jakir <mahfujahmedjakir@gmail.com>
2022-04-23 16:51:23 +0600

    first release

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    'use strict';
+
+// auto slider js start ==========================================================
+$('#banner_slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: '<i class="fas fa-chevron-left prv_arrr"></i>',
+  nextArrow: '<i class="fas fa-chevron-right nxt_arrr"></i>',
+  dots: true,
+});
+
+
+
+
+
+
+
+
+
+
+});
